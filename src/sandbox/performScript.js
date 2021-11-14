8a4647232086b6e544eabdc25bf96745f22d2ad9
odaneo <odaneo@outlook.com>
2021-11-14 12:13:10 +0800

    feat: init

---
+++ src/sandbox/performScript.js
@@ -0,0 +1,28 @@
+// 执行应用的 js 内容 new Function 篇
+export const performScript = (script, appName, global) => {
+  const scriptText =
+    `try {
+       ${script}
+       return window['${appName}']
+      } catch (err) {
+          console.error('runScript error:' + err);
+      }`;
+
+  const performer = new Function(scriptText);
+  return performer.call(global, global);
+}
+
+// 执行应用中的 js 内容 eval篇
+export const performScriptForEval = (script, appName, global) => {
+  const scriptText = `
+    (() => () => {
+      try {
+        ${script}
+        return window['${appName}']
+      } catch (err) {
+        console.error('runScript error:' + err);
+      }
+    })()
+  `
+  return (() => eval(scriptText))().call(global, global)
+}
